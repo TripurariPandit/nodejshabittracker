0746b0a1a50a34e658afb84da71f08799f0971d3
Tripurari Pandit <tripurarikumar766@gmail.com.com>
2023-07-24 14:00:44 +0530

    db setting

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,14 +5,14 @@ const dbUsername = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DBNAME;
 
-// const dbURL = 'mongodb+srv://tripurari:Tripurari@cluster0.8j7vazq.mongodb.net/mernstack?retryWrites=true&w=majority';
+const dbURL = 'mongodb+srv://tripurari:Tripurari@cluster0.8j7vazq.mongodb.net/mernstack?retryWrites=true&w=majority';
 
 // const dbURL = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.8j7vazq.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 
-const dbURL = dbUsername && dbPassword && dbName
-  ? `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.8j7vazq.mongodb.net/${dbName}?retryWrites=true&w=majority`
-  : 'mongodb://127.0.0.1/habitTracker';
+// const dbURL = dbUsername && dbPassword && dbName
+//   ? `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.8j7vazq.mongodb.net/${dbName}?retryWrites=true&w=majority`
+//   : 'mongodb://127.0.0.1/habitTracker';
 
 mongoose.connect(dbURL)
   .then(() => {
